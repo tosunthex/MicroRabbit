6592c96c9bda0ca3e6bf9443dec858864e77da24
Levent Saatçi <leventsaatci@hotmail.com>
2020-10-26 21:42:36 +0100

    Implemented Publish and SendCommand for banking microservices

--- MicroRabbit/MicroServices/Banking/Api/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroServices/Banking/Api/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,13 +23,13 @@ namespace MicroRabbit.Banking.Api.Controllers
         {
             return Ok(_accountService.GetAccounts());
         }
-        /*
+        
         [HttpPost]
         public IActionResult Post([FromBody] AccountTransfer accountTransfer)
         {
             _accountService.Transfer(accountTransfer);
             return Ok(accountTransfer);
         }
-        */
+        
     }
 }
