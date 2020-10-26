54aff22dad5a3052bb7657cc5a6dbcc994c219df
Levent Saatçi <leventsaatci@hotmail.com>
2020-10-26 11:56:04 +0100

    Partiallay completed Banking Microservice

---
+++ MicroRabbit/MicroServices/Banking/Application/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
